7d24effb51b03fd62c84d8421801ccb9246c27d4
WilliamEiji <william.fujimori@gmail.com>
2022-04-03 15:02:29 -0300

    projeto pronto

--- scripts.js
+++ scripts.js
@@ -18,75 +18,66 @@ let enderecoCliente = "";
 let mensagemCodi = "";
 let mensagemPronta = "";
 
+
+let condComida = "";
+let condBebida = "";
+let condPudim = "";
+
 function estaSelecionadoComida(botaoComida){
     const botaoSelecionado = document.querySelector(".selecionado");
-
     if (botaoSelecionado !== null){
     botaoSelecionado.classList.toggle("selecionado");
-    }
+    condComida = botaoComida.classList.contains("selecionado");
+     }
     else{
     botaoComida.classList.add("selecionado");
+    condComida = botaoComida.classList.contains("selecionado");
     nomeComida = botaoComida.querySelector(".comidas").innerHTML;
     document.querySelector(".produtoSelecionadoComida").innerHTML = nomeComida;
     valorComida = botaoComida.querySelector(".valoresc").innerHTML;
-
     valorComida = valorComida.replace("R$ ", "");
-
     document.querySelector(".valorSelecionadoComida").innerHTML = valorComida;
-
     valorComida = valorComida.replace(",", ".");
-    console.log(nomeComida)
-    console.log(valorComida)
-    verificarItens()
     }
+    verificarItens()
 }
 
 function estaSelecionadoBebida(botaoBebida){
     const botaoSelecionado = document.querySelector(".selecionado1");
-
     if (botaoSelecionado !== null){
-        botaoSelecionado.classList.remove("selecionado1");
+        botaoSelecionado.classList.toggle("selecionado1");
+        condBebida = botaoBebida.classList.contains("selecionado1");
     }
     else {
     botaoBebida.classList.add("selecionado1");
+    condBebida = botaoBebida.classList.contains("selecionado1");
     nomeBebida = botaoBebida.querySelector(".bebidas").innerHTML;
     document.querySelector(".produtoSelecionadoBebida").innerHTML = nomeBebida;
     valorBebida = botaoBebida.querySelector(".valoresb").innerHTML;
-
     valorBebida = valorBebida.replace("R$ ", "");
-
     document.querySelector(".valorSelecionadoBebida").innerHTML = valorBebida;
-
     valorBebida = valorBebida.replace(",", ".");
-    console.log(nomeBebida)
-    console.log(valorBebida)
-    verificarItens()
     }
+    verificarItens()
 }
 
 function estaSelecionadoPudim(botaoPudim){
     const botaoSelecionado = document.querySelector(".selecionado2");
-
     if (botaoSelecionado !== null){
-        botaoSelecionado.classList.remove("selecionado2");
+        botaoSelecionado.classList.toggle("selecionado2");
+        condPudim = botaoPudim.classList.contains("selecionado2");
     }
     else{
     botaoPudim.classList.add("selecionado2");
+    condPudim = botaoPudim.classList.contains("selecionado2");
     nomePudim = botaoPudim.querySelector(".pudim").innerHTML;
     document.querySelector(".produtoSelecionadoPudim").innerHTML = nomePudim;
     valorPudim = botaoPudim.querySelector(".valoresp").innerHTML;
-    
-
     valorPudim = valorPudim.replace("R$ ", "");
-
-    
     document.querySelector(".valorSelecionadoPudim").innerHTML = valorPudim;
-
     valorPudim = valorPudim.replace(",", ".");
-    console.log(nomePudim)
-    console.log(valorPudim)
-    verificarItens()
     }
+    verificarItens()
 }
 
 function trocarValores(){
@@ -99,13 +90,17 @@ function trocarValores(){
 
 function verificarItens(){
     const estaCerto = document.querySelector(".fecharPedido");
-    if(nomeComida && nomeBebida  && nomePudim){
+    if(condComida === true && condBebida === true  && condPudim === true){
     trocarValores();
     estaCerto.innerHTML = "Fechar pedido";
     estaCerto.classList.add("botaoVerificado");
-    
+    estaCerto.classList.remove("botaoRecusado")
     pedidoOk = 1;
-}
+    }else {
+        estaCerto.innerHTML = "Selecione os 3 itens para fechar o pedido";
+        estaCerto.classList.add("botaoRecusado")
+        estaCerto.classList.remove("botaoVerificado");
+    }
 }
 
 function finalizarPedido(){
@@ -113,6 +108,11 @@ function finalizarPedido(){
     if(pedidoOk === 1){
         nomeCLiente = prompt("Qual seu nome?");
         enderecoCliente = prompt("Qual seu endereço?")
+    }
+    if (nomeCLiente === null && enderecoCliente === null){
+        alert("Por favor adicione Nome e Endereço")
+        finalizarPedido()
+    }else{
         finaliza.classList.remove("ocultar");
     }
 }
@@ -121,15 +121,13 @@ function botaoCancelar(){
     window.location.reload();
 } 
 
-
-
 function enviarPedido(){
 mensagemPronta = 
 `Olá, gostaria de fazer o pedido:
 - Prato: ${nomeComida}
 - Bebida: ${nomeBebida}
 - Sobremesa: ${nomePudim}
-Total: R$ ${valorTotal}
+Total: R$ ${valorTotal.toFixed(2)}
 
 Nome: ${nomeCLiente}
 Endereço: ${enderecoCliente}`;
