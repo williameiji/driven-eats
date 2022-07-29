ab5f0adbca65828797186886a9b96b910269ba96
williameiji <william.fujimori@gmail.com>
2022-07-29 09:59:41 -0300

    refactor: semantic tags and indentation

--- scripts.js
+++ scripts.js
@@ -1,101 +1,94 @@
-
 let nomeComida;
 let nomeBebida;
 let nomePudim;
-
 let pedidoOk = 0 ;
-
 let valorComida = "";
 let valorBebida = "";
 let valorPudim = "" ;
 let valorTotal = "";
-
 let comidase = "";
-
 let nomeCLiente = "";
 let enderecoCliente = "";
-
 let mensagemCodi = "";
 let mensagemPronta = "";
-
-
 let condComida = "";
 let condBebida = "";
 let condPudim = "";
 
 function estaSelecionadoComida(botaoComida){
     const botaoSelecionado = document.querySelector(".selecionado");
+
     if (botaoSelecionado !== null){
-    botaoSelecionado.classList.toggle("selecionado");
-    condComida = botaoComida.classList.contains("selecionado");
-     }
-    else{
-    botaoComida.classList.add("selecionado");
-    condComida = botaoComida.classList.contains("selecionado");
-    nomeComida = botaoComida.querySelector(".comidas").innerHTML;
-    document.querySelector(".produtoSelecionadoComida").innerHTML = nomeComida;
-    valorComida = botaoComida.querySelector(".valoresc").innerHTML;
-    valorComida = valorComida.replace("R$ ", "");
-    document.querySelector(".valorSelecionadoComida").innerHTML = valorComida;
-    valorComida = valorComida.replace(",", ".");
+        botaoSelecionado.classList.toggle("selecionado");
+        condComida = botaoComida.classList.contains("selecionado");
+     }else{
+        botaoComida.classList.add("selecionado");
+        condComida = botaoComida.classList.contains("selecionado");
+        nomeComida = botaoComida.querySelector(".comidas").innerHTML;
+        document.querySelector(".produtoSelecionadoComida").innerHTML = nomeComida;
+        valorComida = botaoComida.querySelector(".valoresc").innerHTML;
+        valorComida = valorComida.replace("R$ ", "");
+        document.querySelector(".valorSelecionadoComida").innerHTML = valorComida;
+        valorComida = valorComida.replace(",", ".");
     }
+
     verificarItens()
 }
 
 function estaSelecionadoBebida(botaoBebida){
     const botaoSelecionado = document.querySelector(".selecionado1");
+
     if (botaoSelecionado !== null){
         botaoSelecionado.classList.toggle("selecionado1");
         condBebida = botaoBebida.classList.contains("selecionado1");
+    }else {
+        botaoBebida.classList.add("selecionado1");
+        condBebida = botaoBebida.classList.contains("selecionado1");
+        nomeBebida = botaoBebida.querySelector(".bebidas").innerHTML;
+        document.querySelector(".produtoSelecionadoBebida").innerHTML = nomeBebida;
+        valorBebida = botaoBebida.querySelector(".valoresb").innerHTML;
+        valorBebida = valorBebida.replace("R$ ", "");
+        document.querySelector(".valorSelecionadoBebida").innerHTML = valorBebida;
+        valorBebida = valorBebida.replace(",", ".");
     }
-    else {
-    botaoBebida.classList.add("selecionado1");
-    condBebida = botaoBebida.classList.contains("selecionado1");
-    nomeBebida = botaoBebida.querySelector(".bebidas").innerHTML;
-    document.querySelector(".produtoSelecionadoBebida").innerHTML = nomeBebida;
-    valorBebida = botaoBebida.querySelector(".valoresb").innerHTML;
-    valorBebida = valorBebida.replace("R$ ", "");
-    document.querySelector(".valorSelecionadoBebida").innerHTML = valorBebida;
-    valorBebida = valorBebida.replace(",", ".");
-    }
+
     verificarItens()
 }
 
 function estaSelecionadoPudim(botaoPudim){
     const botaoSelecionado = document.querySelector(".selecionado2");
+
     if (botaoSelecionado !== null){
         botaoSelecionado.classList.toggle("selecionado2");
         condPudim = botaoPudim.classList.contains("selecionado2");
+    }else{
+        botaoPudim.classList.add("selecionado2");
+        condPudim = botaoPudim.classList.contains("selecionado2");
+        nomePudim = botaoPudim.querySelector(".pudim").innerHTML;
+        document.querySelector(".produtoSelecionadoPudim").innerHTML = nomePudim;
+        valorPudim = botaoPudim.querySelector(".valoresp").innerHTML;
+        valorPudim = valorPudim.replace("R$ ", "");
+        document.querySelector(".valorSelecionadoPudim").innerHTML = valorPudim;
+        valorPudim = valorPudim.replace(",", ".");
     }
-    else{
-    botaoPudim.classList.add("selecionado2");
-    condPudim = botaoPudim.classList.contains("selecionado2");
-    nomePudim = botaoPudim.querySelector(".pudim").innerHTML;
-    document.querySelector(".produtoSelecionadoPudim").innerHTML = nomePudim;
-    valorPudim = botaoPudim.querySelector(".valoresp").innerHTML;
-    valorPudim = valorPudim.replace("R$ ", "");
-    document.querySelector(".valorSelecionadoPudim").innerHTML = valorPudim;
-    valorPudim = valorPudim.replace(",", ".");
-    }
+
     verificarItens()
 }
 
 function trocarValores(){
     valorTotal = Number(valorComida) + Number(valorBebida) + Number(valorPudim);
-    document.querySelector(".valorTotal").innerHTML = `R$ ${valorTotal.toFixed(2)}`;
+    document.querySelector(".valorTotal").innerHTML = `R$ ${parseInt(valorTotal).toFixed(2).replace(".", ",")}`;
 }
-
-
-//FINALIZAR PEDIDO
 
 function verificarItens(){
     const estaCerto = document.querySelector(".fecharPedido");
+
     if(condComida === true && condBebida === true  && condPudim === true){
-    trocarValores();
-    estaCerto.innerHTML = "Fechar pedido";
-    estaCerto.classList.add("botaoVerificado");
-    estaCerto.classList.remove("botaoRecusado")
-    pedidoOk = 1;
+        trocarValores();
+        estaCerto.innerHTML = "Fechar pedido";
+        estaCerto.classList.add("botaoVerificado");
+        estaCerto.classList.remove("botaoRecusado")
+        pedidoOk = 1;
     }else {
         estaCerto.innerHTML = "Selecione os 3 itens para fechar o pedido";
         estaCerto.classList.add("botaoRecusado")
@@ -109,6 +102,7 @@ function finalizarPedido(){
         nomeCLiente = prompt("Qual seu nome?");
         enderecoCliente = prompt("Qual seu endereço?")
     }
+
     if (nomeCLiente === null && enderecoCliente === null){
         alert("Por favor adicione Nome e Endereço")
         finalizarPedido()
@@ -122,16 +116,18 @@ function botaoCancelar(){
 } 
 
 function enviarPedido(){
-mensagemPronta = 
-`Olá, gostaria de fazer o pedido:
-- Prato: ${nomeComida}
-- Bebida: ${nomeBebida}
-- Sobremesa: ${nomePudim}
-Total: R$ ${valorTotal.toFixed(2)}
+    mensagemPronta = 
+    `Olá, gostaria de fazer o pedido:
+    - Prato: ${nomeComida}
+    - Bebida: ${nomeBebida}
+    - Sobremesa: ${nomePudim}
+    Total: R$ ${parseInt(valorTotal).toFixed(2).replace(".", ",")}
 
-Nome: ${nomeCLiente}
-Endereço: ${enderecoCliente}`;
-mensagemCodi = encodeURIComponent(mensagemPronta);
+    Nome: ${nomeCLiente}
+    Endereço: ${enderecoCliente}`;
+
+    mensagemCodi = encodeURIComponent(mensagemPronta);
+
     window.open(`https://wa.me/5543999725094?text=${mensagemCodi}`);
 }
 
